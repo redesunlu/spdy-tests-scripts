62ca69340aed46d0129461ebe6c9b53aec782b63
Nicolas <nicoillia@gmail.com>
2014-04-19 17:26:20 -0300

    cleaning tow calulator code

--- script/Experiment 3/tow.py
+++ script/Experiment 3/tow.py
@@ -1,21 +1,8 @@
 #!/usr/bin/python
 
-from os.path import exists
 from scapy.all import *
-from import argv, exit
 
 
 def calc_tow(captureFile):
     capture = rdpcap(captureFile)
     return capture[len(capture)-1].time - capture[0].time
-
-
-if len(argv) != 2:
-    exit('ERROR: Bad parameters!')
-
-if not exists(argv[1]):
-    exit('ERROR: Directory %s was not found!' % argv[1])
-else:
-    captureFile = argv[1]
-
-print calc_tow(captureFile)
